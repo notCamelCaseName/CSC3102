e2d432989db2aedd1fa5170991ed9f184b965ae0
Louis Delyon <louis.delyon.si@gmail.com>
2022-09-19 15:36:16 +0200

    Fini le DM

--- CI1/DM/decimal_to_binary.sh
+++ CI1/DM/decimal_to_binary.sh
@@ -4,12 +4,8 @@ n=$1
 res=""
 
 while [ $n -ge 2 ]; do
-	res=$res$(expr $n % 2)
+	res=$(expr $n % 2)$res
 	n=$(expr $n / 2)
 done
 
-if [ $(expr $1 % 2) -eq 0 ]; then
-	echo $(./mirror.sh $res$n)0
-else
-	echo $(./mirror.sh $res$n)
-fi
+echo $n$res
